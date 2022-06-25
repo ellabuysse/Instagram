06f4ba8ea3866b4a819f76ae4571930a961ccd81
Ella Buysse <ellabuysse@fb.com>
2022-06-25 15:30:12 -0700

    added signup/login functionality

---
+++ Instagram/View Controllers/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  Instagram
+//
+//  Created by ellabuysse on 6/24/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
